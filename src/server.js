62484fee5af42010b77b08f475f09b27b365d099
Olesia Duna <157960218+OlesiaDuna@users.noreply.github.com>
2024-11-13 21:19:14 +0200

    whole work

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', 3000));
 
@@ -17,6 +18,7 @@ export const setupServer = () => {
   app.use(cookieParser());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
   app.get('/', (req, res) => {
     res.status(200).json({
